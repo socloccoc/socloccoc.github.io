90a5abe9e7d2d3dac8787406ee03807cdb9b199b
socloccoc <daihusk57@gmail.com>
2018-10-26 16:56:32 +0700

    chart with d3js

--- services/chartService.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module("candlestick");
-
-app.factory("chartService", ["$http", function($http){
-    return {
-        get : function(){
-            return $http.get("https://coinzukan.xsrv.jp/api/candlestickData");
-        }
-    }
-}]);
