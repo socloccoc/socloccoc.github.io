0fb9ee707ef1afd29a2848735069b4c0c2cd187a
socloccoc <daihusk57@gmail.com>
2018-10-30 19:15:00 +0700

    upload chart v3

---
+++ app/landing/landing.controller.js
@@ -0,0 +1,14 @@
+
+(function () {
+	'use strict';
+
+	angular.module('app.landing')
+		.controller('LandingController', LandingController);
+
+	LandingController.$inject = ['AuthService'];
+	function LandingController(AuthService) {
+		var vm = this;
+		vm.currentUser = AuthService.currentUser;
+		console.log(vm.currentUser);
+	}
+})();
