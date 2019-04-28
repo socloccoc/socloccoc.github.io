a6182f4b017a8cf450c7128db9ee2a3e4bb00da8
socloccoc <daihusk57@gmail.com>
2019-04-28 23:59:14 +0700

    a

--- datafeeds/udf/src/udf-compatible-datafeed-base.ts
+++ datafeeds/udf/src/udf-compatible-datafeed-base.ts
@@ -348,7 +348,7 @@ function defaultConfiguration(): UdfCompatibleConfiguration {
 	return {
 		supports_search: false,
 		supports_group_request: true,
-		supported_resolutions: ['1', '5', '15', '30', '60', '1D', '1W', '1M'],
+		supported_resolutions: ['1D', '1W', '1M'],
 		supports_marks: false,
 		supports_timescale_marks: false,
 	};
